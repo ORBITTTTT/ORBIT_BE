6cc97dbaf6f5767d13609a63e2823e426ef4d749
Son-Gyeongi <thsruddl77@gmail.com>
2023-04-17 17:46:51 +0900

    init commit

---
+++ src/main/java/tra/orbit_be/OrbitBeApplication.java
@@ -0,0 +1,13 @@
+package tra.orbit_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbitBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrbitBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tra/orbit_be/OrbitBeApplicationTests.java
@@ -0,0 +1,13 @@
+package tra.orbit_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbitBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
