167425bdc519ecc462f3350ab78a2ce1b1e25b88
Son-Gyeongi <thsruddl77@gmail.com>
2023-06-08 20:52:49 +0900

    extra. provider 추가 - FormLoginAuthProvider - JWTAuthProvider

---
+++ src/main/java/tra/orbit_be/security/provider/JWTAuthProvider.java
@@ -0,0 +1,42 @@
+package tra.orbit_be.security.provider;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+import tra.orbit_be.exception.CustomException;
+import tra.orbit_be.exception.ErrorCode;
+import tra.orbit_be.login.model.User;
+import tra.orbit_be.login.repository.OAuthRepository;
+import tra.orbit_be.security.UserDetailsImpl;
+import tra.orbit_be.security.jwt.JwtDecoder;
+import tra.orbit_be.security.jwt.JwtPreProcessingToken;
+
+@Component
+@RequiredArgsConstructor
+public class JWTAuthProvider implements AuthenticationProvider {
+
+    private final JwtDecoder jwtDecoder;
+    private final OAuthRepository oAuthRepository;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String token = (String) authentication.getPrincipal();
+        String username = jwtDecoder.decodeUsername(token);
+
+        User user = oAuthRepository.findByUserEmail(username)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        UserDetailsImpl userDetails = new UserDetailsImpl(user);
+
+        return new UsernamePasswordAuthenticationToken(
+                userDetails, null, userDetails.getAuthorities()
+        );
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return JwtPreProcessingToken.class.isAssignableFrom(authentication);
+    }
+}
