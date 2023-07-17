bb8a504828c522e11345004d1b39a4c1af47485b
joo-hui <02hjooo@gmail.com>
2023-07-17 22:09:08 +0900

    save. 게시글 작성 진행중

--- src/main/java/tra/orbit_be/service/ProjectService.java
+++ src/main/java/tra/orbit_be/service/ProjectService.java
@@ -5,12 +5,14 @@ import org.springframework.stereotype.Service;
 import tra.orbit_be.domain.Project;
 import tra.orbit_be.dto.ProjectRequestDto;
 import tra.orbit_be.repository.ProjectRepository;
+import tra.orbit_be.security.UserDetailsImpl;
 
 @Service
 @RequiredArgsConstructor
 public class ProjectService {
 
     private final ProjectRepository projectRepository;
+    private final UserService userService;
 
     // 프로젝트 생성(게시글 생성)
     public Project createProject(ProjectRequestDto projectRequestDto) {
